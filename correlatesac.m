1e2465d44648c8f235814b78f6fde41c30fc7c0c
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-09-29 10:33:44 -0400

    Added options to adjust window length

--- correlatesac.m
+++ correlatesac.m
@@ -1,9 +1,14 @@
-function [cc, tr, hdr] = correlatesac(allsacfiles, sortcriteria, arrcriteria, slide, plt)
-% [ccs, tr, hdr] = CORRELATESAC(allsacfiles, sortcriteria, arrcriteria, slide, plt)
+function [cc, tr, hdr] = correlatesac(allsacfiles, sortcriteria, arrcriteria, slide, win, plt, fmt)
+% [ccs, tr, hdr] = CORRELATESAC(allsacfiles, sortcriteria, arrcriteria, slide, win, plt)
 %
 % Computes and plots the correlation coefficients between seismograms for
-% the first 5 seconds after the first picked P-wave arrivals of all
-% sac files.
+% the windows of all sac files. Use slide and win to adjust the beginning 
+% and the end of the window. Slide can be either a scalar or vector.
+% For the later case, the correlation coefficient output will be a 3D array
+% with following indices: 
+%       cc(slide_index, station1_index, station2_index).
+% If plt is set to true, a PDF figure and a video are produced when slide
+% is a scalar and a vector, respectively.
 %
 % INPUT
 % allsacfiles       cell array to full path to all sac files
@@ -12,6 +17,7 @@ function [cc, tr, hdr] = correlatesac(allsacfiles, sortcriteria, arrcriteria, sl
 % arrcriteria       1 -- pick                [Default]
 %                   2 -- predicted (ak135)
 % slide             amount of window slide   [Default: 0]
+% win               window length            [Default: 5]
 % plt               whether to plot or not   [Default: true]
 %
 % OUTPUT
@@ -19,22 +25,70 @@ function [cc, tr, hdr] = correlatesac(allsacfiles, sortcriteria, arrcriteria, sl
 % tr                seismic traces, all sorted
 % hdr               sac headers, all sorted
 %
-% Last modified by sirawich-at-princeton.edu, 09/27/2021
+% Last modified by sirawich-at-princeton.edu, 09/29/2021
 
 defval('sortcriteria', 1)
 defval('arrcriteria', 1)
 defval('slide', 0)
+defval('win', 5)
 defval('plt', true)
+defval('fmt', 'eps')
 
-% limits of the time window of the seismograms
 if length(slide) > 1
-    for ii = 1:length(slide)
-        [cc, tr, hdr] = correlatesac(allsacfiles, sortcriteria, arrcriteria, slide(ii), plt);
+    images = {};
+    % run the first slide value to check the number of valid station
+    try
+        [cc_slide, tr, hdr] = correlatesac(allsacfiles, sortcriteria, ...
+            arrcriteria, slide(1), plt, 'jpg');
+        cc = zeros(length(slide), size(cc_slide, 1), size(cc_slide, 2));
+        cc(1,:,:) = cc_slide;
+        
+        % limits of the time window of the seismograms
+        window_left = 0 + slide(1);
+        window_right = win + slide(1);
+        
+        eqid = hdr{1,1}.USER7;
+        images{1} = sprintf('%s%s_%d%d_%d_%.1fs_%.1fs_ccplot.jpg', ...
+            getenv('EPS'), mfilename, sortcriteria, arrcriteria, eqid, ...
+            window_left, window_right);
+    catch
+        return
+    end
+    % run the remaining slides
+    for ii = 2:length(slide)
+        try
+            [cc_slide, tr, hdr] = correlatesac(allsacfiles, sortcriteria, ...
+                arrcriteria, slide(ii), plt, 'jpg');
+            cc(ii,:,:) = cc_slide;
+            
+            % limits of the time window of the seismograms
+            window_left = 0 + slide(ii);
+            window_right = win + slide(ii);
+            
+            eqid = hdr{1,1}.USER7;
+            images{ii} = sprintf('%s%s_%d%d_%d_%.1fs_%.1fs_ccplot.jpg', ...
+                getenv('EPS'), mfilename, sortcriteria, arrcriteria, eqid, ...
+                window_left, window_right);
+        catch
+            continue
+        end
+    end
+    
+    % convert saved images to a video
+    videofilename = sprintf('%s_%d%d_%d_%.1fs_%.1fs_ccplot', ...
+                mfilename, sortcriteria, arrcriteria, eqid, ...
+                slide(1), slide(end));
+    images2video(images, 'jpg', 10, videofilename);
+    
+    % remove the intermediate jpg files
+    for ii = 1:length(images)
+        system(sprintf('rm %s', images{ii}));
     end
     return
 else
+    % limits of the time window of the seismograms
     window_left = 0 + slide;
-    window_right = 5 + slide;
+    window_right = win + slide;
 end
 
 n = length(allsacfiles);
@@ -71,7 +125,7 @@ for ii = 1:n
     % filter out the ambient noise below 1 Hz
     x = bandpass(x, fs, 1, 2, 2, 1, 'butter', 'linear');
     
-    % cut sections between -10 and 5 s from picked arrival time
+    % cut sections between [window_left window_right] from picked arrival time
     if arrcriteria == 1
         wh = and(tims >= HdrData.T0 + window_left, ...
             tims <= HdrData.T0 + window_right);
@@ -141,10 +195,13 @@ for ii = 1:n
     end
 end
 
+%% plot the correlation table
 if plt
     figure(3)
-    set(gcf, 'Units', 'inches', 'Position', [8 8 6 4.8])
+    set(gcf, 'Units', 'inches', 'Position', [8 8 6 6])
     clf
+    subplot('Position', [0.14 0.12 0.8 0.74]);
+    cla
     imagesc(cc, [-1 1]);
     colorbar
     colormap('jet');
@@ -158,7 +215,13 @@ if plt
     eqid = hdr{1,1}.USER7;
     mag = hdr{1,1}.MAG;
     evdp = hdr{1,1}.EVDP;
-    title(sprintf('Event ID: %d, Magnitude: %4.2f, Depth: %6.2f km', eqid, mag, evdp));
+    if arrcriteria == 1
+        arrstr = 'picked';
+    else
+        arrstr = 'ak135';
+    end
+    title(sprintf('Event ID: %d, window: %.2f to %.2f s around %s arrival', ...
+        eqid, window_left, window_right, arrstr));
     
     ax2 = doubleaxes(gca);
     ax2.YAxis.Visible = 'off';
@@ -167,7 +230,16 @@ if plt
     else
         ax2.XTickLabel = string(round(azs, 1));
     end
+    ax2.XTickLabelRotation = 45;
     
-    filename = sprintf('%s_%d_ccplot.eps', mfilename, eqid);
-    figdisp(filename, [], [], 2, [], 'epstopdf');
+    filename = sprintf('%s_%d%d_%d_%.1fs_%.1fs_ccplot', ...
+        mfilename, sortcriteria, arrcriteria, eqid, window_left, ...
+        window_right);
+    if strcmp(fmt, 'jpg')
+        filename = strcat(filename, '.jpg');
+        print([getenv('EPS') filename], '-djpeg', '-r300');
+    else
+        filename = strcat(filename, '.eps');
+        figdisp(filename, [], [], 2, [], 'epstopdf');
+    end
 end
