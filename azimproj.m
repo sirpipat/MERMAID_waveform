77c17f519addf5123731668626bac510e9a3022a
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-10-12 10:36:14 -0400

    Additional documentation

--- azimproj.m
+++ azimproj.m
@@ -7,6 +7,7 @@ function [azDeg, distDeg] = azimproj(center, pts)
 % INPUT:
 % center        [lon lat] of the center of the azimuthal map projection
 % pts           [lons lats] of the points to be projected
+%               lons and lats may be column vectors of equal length
 %
 % OUTPUT:
 % azDeg         azimuths in degrees of the projected points
@@ -15,11 +16,12 @@ function [azDeg, distDeg] = azimproj(center, pts)
 % SEE ALSO:
 % AZIM, GRCDIST, AZIMDIST
 %
-% Last modified by sirawich-at-princeton.edu, 10/11/2021
+% Last modified by sirawich-at-princeton.edu, 10/12/2021
 
 azDeg = zeros(size(pts,1), 1);
 distDeg = zeros(size(pts,1), 1);
 
+% find azimuth and distance from the center of map projection
 for ii = 1:size(pts, 1)
     [azDeg(ii, 1), ~, distDeg(ii, 1)] = azimdist(center, pts(ii, :));
 end
