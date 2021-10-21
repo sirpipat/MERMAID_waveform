13bad88997175f4c22d4ebc674d91057d0370842
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-10-21 13:25:57 -0400

    Added more figures

--- obssynshift.m
+++ obssynshift.m
@@ -22,13 +22,19 @@ function [t_shift, CCmax, lag, CC, x_o, x_s, dts] = obssynshift(obs, syn)
 % SEE ALSO:
 % CCSHIFT
 %
-% Last modified by sirawich-at-princeton.edu, 10/19/2021
+% Last modified by sirawich-at-princeton.edu, 10/20/2021
+
+badval = -12345;
 
 % read the seismogram
 [SeisData_o, HdrData_o, ~, ~, tims_o] = readsac(obs);
 [SeisData_s, HdrData_s, ~, ~, tims_s] = readsac(syn);
 [dt_ref_o, dt_begin_o, dt_end_o, fs_o, npts_o, dts_o, ~] = gethdrinfo(HdrData_o);
 [dt_ref_s, dt_begin_s, dt_end_s, fs_s, npts_s, dts_s, ~] = gethdrinfo(HdrData_s);
+
+if isnan(HdrData_o.T0) || HdrData_o.T0 == badval
+    return
+end
 
 % remove instrument response + filter the observed seismogram
 x_o = real(counts2pa(SeisData_o));
@@ -52,30 +58,91 @@ x_s = x_s(wh);
 x_o_e = envelope(x_o);
 x_s_e = envelope(x_s);
 
-% find best timeshift
-[t_shift, CCmax, lag, CC] = ccshift(x_o_e, x_s_e, dts(1), dts(1), ...
-    fs_o, seconds(50)); % half window size is 100 and I remove the first 10 seconds due to the artifact from filtering
+% find best timeshift using envelopes
+[CC_e, lag_e] = xcorr(detrend(x_o_e, 1), detrend(x_s_e, 1), 'coeff');
+lag_e = lag_e * HdrData_o.DELTA;
+[CCmax_e, index] = max(CC_e);
+t_shift_e = lag_e(index);
+% [t_shift_e, CCmax_e, lag_e, CC_e] = ccshift(x_o_e, x_s_e, dts(1), dts(1), ...
+%     fs_o, seconds(50)); % half window size is 100 and I remove the first 10 seconds due to the artifact from filtering
 
+% find best timeshift using original waveforms
+[CC, lag] = xcorr(detrend(x_o, 1), detrend(x_s, 1), 'coeff');
+lag = lag * HdrData_o.DELTA;
+[CCmax, index] = max(CC);
+t_shift = lag(index);
+% [t_shift, CCmax, lag, CC] = ccshift(x_o, x_s, dts(1), dts(1), ...
+%     fs_o, seconds(50)); % half window size is 100 and I remove the first 10 seconds due to the artifact from filtering
+
+%% plot the result
 figure(7)
+set(gcf, 'Renderer', 'painters', 'Units', 'inches', 'Position', ...
+    [2 6 12 8]);
 clf
 % plot the lag
-ax1 = subplot(2, 1, 1);
-plot(lag, CC, 'k', 'LineWidth', 1)
+ax1 = subplot('Position', [0.07 0.72 0.88 0.23], 'FontSize', 12);
+plot(lag, CC, 'Color', [0.5 0.5 0.5], 'LineWidth', 1)
 hold on
 grid on
+box on
 xlabel('lag time (s)')
 ylabel('correlation coefficient')
-scatter(t_shift, CCmax, 80, 'Marker', 'v', 'MarkerEdgeColor', 'k', ...
+plot(lag_e, CC_e, 'k', 'LineWidth', 1)
+scatter(t_shift_e, CCmax_e, 80, 'Marker', 'v', 'MarkerEdgeColor', 'k', ...
     'MarkerFaceColor', 'b');
+scatter(t_shift, CCmax, 80, 'Marker', 'v', 'MarkerEdgeColor', 'k', ...
+    'MarkerFaceColor', 'r', 'MarkerEdgeAlpha', 0.5, ...
+    'MarkerFaceAlpha', 0.5);
+legend('original CC', 'envelope CC', 'Location', 'best')
+ax1.XLim = [-100 100];
+title(ax1, sprintf('best shift: original = %.2f s, envelope = %.2f s', ...
+    t_shift, t_shift_e))
 
-% plot the seismogram
-ax2 = subplot(2, 1, 2);
+% plot the shifted seimograms using best timeshift from original waveforms
+ax2 = subplot('Position', [0.07 0.38 0.88 0.23], 'FontSize', 12);
 plot(dts, x_o / max(abs(x_o)), 'k', 'LineWidth', 1)
 hold on
 grid on
+box on
 plot(dts + seconds(t_shift), x_s / max(abs(x_s)), 'b', 'LineWidth', 2)
 plot(dts,  x_o_e / max(abs(x_o)), 'Color', [0.7 0.7 0.7], 'LineWidth', 1)
 plot(dts + seconds(t_shift),  x_s_e / max(abs(x_s)), 'Color', [0.3 0.7 1], 'LineWidth', 1)
 vline(ax2, dt_ref_o + seconds(HdrData_o.T0), 'LineWidth', 2, ...
     'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
+ax2.XLim = dt_ref_o + seconds(HdrData_o.T0) + seconds([-10 30]);
+legend('observed', 'synthetic -- shifted', 'observed envelope', ...
+    'synthetic envelope -- shifted', 'Location', 'northeast');
+title('applied best timeshift for original waveforms')
+
+% plot the shifted seismogram using best timeshift from envelopes
+ax3 = subplot('Position', [0.07 0.05 0.88 0.23], 'FontSize', 12);
+plot(dts, x_o / max(abs(x_o)), 'k', 'LineWidth', 1)
+hold on
+grid on
+box on
+plot(dts + seconds(t_shift_e), x_s / max(abs(x_s)), 'b', 'LineWidth', 2)
+plot(dts,  x_o_e / max(abs(x_o)), 'Color', [0.7 0.7 0.7], 'LineWidth', 1)
+plot(dts + seconds(t_shift_e),  x_s_e / max(abs(x_s)), 'Color', [0.3 0.7 1], 'LineWidth', 1)
+vline(ax3, dt_ref_o + seconds(HdrData_o.T0), 'LineWidth', 2, ...
+    'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
+ax3.XLim = dt_ref_o + seconds(HdrData_o.T0) + seconds([-10 30]);
+legend('observed', 'synthetic -- shifted', 'observed envelope', ...
+    'synthetic envelope -- shifted', 'Location', 'northeast');
+title('applied best timeshift for envelope functions')
+
+% save the figure to $EPS
+savename = sprintf('%s_%d_%s', mfilename, HdrData_o.USER7, ...
+    replace(HdrData_o.KSTNM, ' ', ''));
+% check whether the filename is already taken
+if exist([getenv('EPS') savename '.pdf'], 'file')
+    % append 01, 02, ... if the filename is already taken
+    ii = 1;
+    savename_plain = savename;
+    savename = sprintf('%s_%02d', savename_plain, ii);
+    while and(exist([getenv('EPS') savename '.pdf'], 'file'), ii < 99)
+        ii = ii + 1;
+        savename = sprintf('%s_%02d', savename_plain, ii);
+    end
+end
+figdisp([savename '.eps'], [], [], 2, [], 'epstopdf');
 end
