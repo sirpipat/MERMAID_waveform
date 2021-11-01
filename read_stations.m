6dbbf644633fb3d176808e0e338e5cfad516f50a
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-11-01 13:20:14 -0400

    Fixed a bug when STATION has only one entry

--- read_stations.m
+++ read_stations.m
@@ -13,11 +13,21 @@ function [n, name, network, x, z] = read_stations(fname)
 % x             x-coordinates
 % z             z-coordinates
 %
-% Last modified by Sirawich Pipatprathanporn, 02/24/2021
+% Last modified by Sirawich Pipatprathanporn, 11/01/2021
 
 % read the station file as a table
 opts = detectImportOptions(fname);
 T = readtable(fname, opts);
+
+% if the table is empty, assume the table has only one entry
+if isempty(T)
+    fid = fopen(fname, 'r');
+    line = fgetl(fid);
+    words = split(line);
+    T = struct('Var1', {words(1)}, 'Var2', {words(2)}, ...
+        'Var3', str2double(words{3}), ...
+        'Var4', str2double(words{4}));
+end
 
 name = T.Var1;
 network = T.Var2;
