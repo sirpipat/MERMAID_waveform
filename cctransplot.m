7f61b2cae37f55fadc4ba541ba742dc2c53b169a
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2021-11-16 17:07:59 -0500

    Added option to plot/not plot and set window function to shanning with 1% tapering

--- cctransplot.m
+++ cctransplot.m
@@ -1,5 +1,5 @@
-function [t_cc, cc, t_rf, rf] = cctransplot(ddir1, ddir2, example, channel1, channel2)
-% CCTRANSPLOT(ddir1, ddir2, example, channel1, channel2)
+function [t_cc, cc, t_rf, rf] = cctransplot(ddir1, ddir2, example, channel1, channel2, plt)
+% CCTRANSPLOT(ddir1, ddir2, example, channel1, channel2, plt)
 %
 % Plot the transfer function and cross correlation coefficient between the
 % z-component displacement at the ocean bottom seismometer and the pressure
@@ -22,6 +22,8 @@ function [t_cc, cc, t_rf, rf] = cctransplot(ddir1, ddir2, example, channel1, cha
 %           'displacement'
 %           'pressure'
 %
+% plt           whether to plot or not [Default: true]
+%
 % OUTPUT:
 % t_cc      time for cross correlation coefficient
 % cc        cross correlation coefficient
@@ -31,10 +33,11 @@ function [t_cc, cc, t_rf, rf] = cctransplot(ddir1, ddir2, example, channel1, cha
 % SEE ALSO:
 % SPECFEM2D_INPUT_SETUP_FLAT, RUNFLATSIM
 % 
-% Last modified by sirawich-at-princeton.edu, 11/15/2021
+% Last modified by sirawich-at-princeton.edu, 11/16/2021
 
 defval('channel1', {'bottom' 'displacement'})
 defval('channel2', {'hydrophone' 'pressure'})
+defval('plt', true)
 
 % read the first arrival of the input signal
 if strcmpi(channel1{2}, 'displacement')
@@ -69,8 +72,8 @@ N = length(tims_i);
 nf = 2 ^ nextpow2(N);
 
 % tapering window
-%w = hann(length(tims_o))';
-w = ones(size(tims_i));
+w = shanning(length(tims_o), 0.01)';
+% w = ones(size(tims_i));
 % w(1:10) = 0;
 % w(end-9:end) = 0;
 
@@ -78,76 +81,82 @@ w = ones(size(tims_i));
 [cc, lags] = xcorr(seisdata_o .* w, seisdata_i .* w, 'coeff');
 t_cc = lags * dt;
 
-% compute the transfer function
-SEISDATA_O = fft(seisdata_i .* w, nf);
-SEISDATA_H = fft(seisdata_o .* w, nf);
-
-% Use damping factor
-% as 1000 times input-signal's amplitude squared (asq)
 asq = max(abs(seisdata_i)).^2;
-d = max(abs(seisdata_i)).^2 * 10;
 
-% compute the response
-rf = ifft(SEISDATA_H .* conj(SEISDATA_O) ./ ...
-    max((SEISDATA_O .* conj(SEISDATA_O)), d), nf);
-rf = rf(1:N);
+[rf, x_h_tran, n, d] = spectraldivision(seisdata_o, seisdata_i, w, 'damp', '');
 t_rf = (0:(N-1)) * dt;
 
-% apply response to obtain hydrophone pressure
-x_h_tran = conv(seisdata_i, rf);
-x_h_tran = x_h_tran(1:length(seisdata_o));
+% % compute the transfer function
+% SEISDATA_O = fft(seisdata_i .* w, nf);
+% SEISDATA_H = fft(seisdata_o .* w, nf);
+% 
+% % Use damping factor
+% % as 1000 times input-signal's amplitude squared (asq)
+% 
+% d = max(abs(seisdata_i)).^2 * 10;
+% 
+% % compute the response
+% rf = ifft(SEISDATA_H .* conj(SEISDATA_O) ./ ...
+%     max((SEISDATA_O .* conj(SEISDATA_O)), d), nf);
+% rf = rf(1:N);
+% t_rf = (0:(N-1)) * dt;
+% 
+% % apply response to obtain hydrophone pressure
+% x_h_tran = conv(seisdata_i, rf);
+% x_h_tran = x_h_tran(1:length(seisdata_o));
 
-figure(1)
-clf
-set(gcf, 'Units', 'inches', 'Position', [0 6 6 9])
+if plt
+    figure(1)
+    clf
+    set(gcf, 'Units', 'inches', 'Position', [0 6 6 9])
 
-ax1 = subplot('Position', [0.08 0.86 0.86 0.12]);
-plot(tims_i, seisdata_i, 'Color', 'k')
-xlim([tims_i(1) tims_i(end)])
-grid on
-xlabel('time (s)')
-title(sprintf('First P-wave arrival, %s, %s', channel1{2}, channel1{1}))
-set(ax1, 'Box', 'on', 'TickDir', 'both');
+    ax1 = subplot('Position', [0.08 0.86 0.86 0.12]);
+    plot(tims_i, seisdata_i, 'Color', 'k')
+    xlim([tims_i(1) tims_i(end)])
+    grid on
+    xlabel('time (s)')
+    title(sprintf('First P-wave arrival, %s, %s', channel1{2}, channel1{1}))
+    set(ax1, 'Box', 'on', 'TickDir', 'both');
 
-ax2 = subplot('Position', [0.08 0.66 0.86 0.12]);
-plot(tims_o, seisdata_o, 'Color', 'k')
-xlim([tims_i(1) tims_i(end)])
-grid on
-hold on
-plot(tims_o, x_h_tran, 'Color', [0.9 0.3 0.1])
-xlabel('time (s)')
-title(sprintf('%s record, %s', channel2{2}, channel2{1}))
-legend('observed', 'from response', 'Location', 'northeast')
-set(ax2, 'Box', 'on', 'TickDir', 'both');
+    ax2 = subplot('Position', [0.08 0.66 0.86 0.12]);
+    plot(tims_o, seisdata_o, 'Color', 'k')
+    xlim([tims_i(1) tims_i(end)])
+    grid on
+    hold on
+    plot(tims_o, x_h_tran, 'Color', [0.9 0.3 0.1])
+    xlabel('time (s)')
+    title(sprintf('%s record, %s', channel2{2}, channel2{1}))
+    legend('observed', 'from response', 'Location', 'northeast')
+    set(ax2, 'Box', 'on', 'TickDir', 'both');
 
-ax3 = subplot('Position', [0.08 0.46 0.86 0.12]);
-plot(tims_o, x_h_tran-seisdata_o, 'Color', 'k')
-grid on
-xlim([tims_o(1), tims_o(end)])
-xlabel('time (s)')
-title(sprintf('from response - observed, norm = %0.5g', ...
-    norm(x_h_tran-seisdata_o, 2)))
-set(ax3, 'Box', 'on', 'TickDir', 'both');
+    ax3 = subplot('Position', [0.08 0.46 0.86 0.12]);
+    plot(tims_o, x_h_tran-seisdata_o, 'Color', 'k')
+    grid on
+    xlim([tims_o(1), tims_o(end)])
+    xlabel('time (s)')
+    title(sprintf('from response - observed, norm = %0.5g', n))
+    set(ax3, 'Box', 'on', 'TickDir', 'both');
 
-ax4 = subplot('Position', [0.08 0.26 0.86 0.12]);
-plot(t_cc, cc, 'Color', 'k')
-xlim([tims_i(1) tims_i(end)])
-grid on
-xlabel('time (s)')
-title('Correlation coefficient')
-set(ax4, 'Box', 'on', 'TickDir', 'both');
+    ax4 = subplot('Position', [0.08 0.26 0.86 0.12]);
+    plot(t_cc, cc, 'Color', 'k')
+    xlim([tims_i(1) tims_i(end)])
+    grid on
+    xlabel('time (s)')
+    title('Correlation coefficient')
+    set(ax4, 'Box', 'on', 'TickDir', 'both');
 
-ax5 = subplot('Position', [0.08 0.06 0.86 0.12]);
-plot(t_rf, rf, 'Color', 'k')
-xlim([0 tims_i(end)-tims_i(1)])
-grid on
-xlabel('time (s)')
-title(sprintf('response, damping factor = %0.5g x input amplitude^2', d/asq))
-set(ax5, 'Box', 'on', 'TickDir', 'both');
+    ax5 = subplot('Position', [0.08 0.06 0.86 0.12]);
+    plot(t_rf, rf, 'Color', 'k')
+    xlim([0 tims_i(end)-tims_i(1)])
+    grid on
+    xlabel('time (s)')
+    title(sprintf('response, damping factor = %0.5g x input amplitude^2', d/asq))
+    set(ax5, 'Box', 'on', 'TickDir', 'both');
 
-% save figure
-set(gcf, 'Renderer', 'painters')
-savename = sprintf('%s_%s_in_%s-%s_out_%s-%s.eps', mfilename, example, ...
-    channel1{1}, channel1{2}, channel2{1}, channel2{2});
-figdisp(savename, [], [], 2, [], 'epstopdf');
+    % save figure
+    set(gcf, 'Renderer', 'painters')
+    savename = sprintf('%s_%s_in_%s-%s_out_%s-%s.eps', mfilename, example, ...
+        channel1{1}, channel1{2}, channel2{1}, channel2{2});
+    figdisp(savename, [], [], 2, [], 'epstopdf');
+end
 end
