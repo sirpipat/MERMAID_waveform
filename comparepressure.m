fadb11e5904900d1bb235773d256bdc141b972a7
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2021-12-03 14:18:56 -0500

    Xcorr on envelops of sliced windows then xcorr on the sliced waveforms

--- comparepressure.m
+++ comparepressure.m
@@ -14,7 +14,7 @@ function comparepressure(seis_s, hdr_s, seis_o, hdr_o, seis_r, t_r)
 %
 % OUTPUT:
 %
-% Last modified by sirawich-at-princeton.edu, 11/30/2021
+% Last modified by sirawich-at-princeton.edu, 12/03/2021
 
 % sampling rate
 [~, dt_begin_s, ~, fs_s, ~, dts_s] = gethdrinfo(hdr_s);
@@ -46,8 +46,25 @@ pres_o = real(pres_o);
 pres_o = bandpass(pres_o, fs_o, 0.5, 2, 4, 2, 'butter', 'linear');
 
 % compute the cross correlation
-[best_lags_time, ~, lags_time, cc, s] = ccscale(pres_o, pres_s, ...
-    dt_begin_o, dt_begin_o, fs_o);
+% cut the short windows to do cross correlation
+best_lags_time_e = 0;
+ep = (1 / fs_o) / 100;
+dt_start = dt_ref_o + seconds(hdr_o.T0 - 10);
+dt_end = dt_ref_o + seconds(hdr_o.T0 + 20);
+pres_o1 = pres_o(and(geq(dts_o, dt_start, ep), leq(dts_o, dt_end, ep)));
+pres_s1 = pres_s(and(geq(dts_o + seconds(best_lags_time_e), dt_start, ep), ...
+    leq(dts_o + seconds(best_lags_time_e), dt_end, ep)));
+
+% first correlate by the envelope
+[best_lags_time_e, ~, ~, ~, ~] = ccscale(pres_o1, pres_s1, ...
+    dt_begin_o, dt_begin_o, fs_o, seconds(15), true);
+
+% then correlate by the waveform
+[best_lags_time, ~, lags_time, cc, s] = ccscale(pres_o1, pres_s1, ...
+    dt_start, dt_start + seconds(best_lags_time_e), fs_o, seconds(5), false);
+
+best_lags_time = best_lags_time + best_lags_time_e;
+lags_time = lags_time + best_lags_time_e;
 
 best_lags = round(best_lags_time * fs_o);
 % [cc, lags] = xcorr(pres_o, pres_s, 'coeff');
@@ -68,7 +85,7 @@ hold on
 plot(dts_o'+seconds(best_lags_time), s * pres_s, 'b', 'LineWidth', 1)
 grid on
 xlim(dt_ref_o + seconds(hdr_o.T0 + [-10 40]))
-ylim([-1.1 1.1] * max(max(abs(pres_o)), max(abs(pres_s))))
+ylim([-1.1 1.1] * max(max(abs(pres_o)), max(abs(s * pres_s))))
 vline(ax1, dt_ref_o + seconds(hdr_o.T0), 'LineWidth', 2, ...
     'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
 legend('observed', 'synthetic')
@@ -79,15 +96,27 @@ set(ax1, 'Box', 'on')
 % residue: observed - shifted synthetic
 ax2 = subplot('Position', [0.08 0.34 0.86 0.24]);
 cla
-plot(dts_o', pres_o - circshift(s * pres_s, best_lags), 'k')
+plot(dts_o', pres_o, 'k')
+hold on
+plot(dts_o'+seconds(best_lags_time), s * pres_s, 'b', 'LineWidth', 1)
 grid on
-xlim(dt_ref_o + seconds(hdr_o.T0 + [-10 40]))
-ylim([-1.1 1.1] * max(abs(pres_o - circshift(pres_s, best_lags))));
+xlim([dts_o(1) dts_o(end)])
+ylim([-1.1 1.1] * max(max(abs(pres_o)), max(abs(s * pres_s))))
 vline(ax2, dt_ref_o + seconds(hdr_o.T0), 'LineWidth', 2, ...
     'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
-title('residue')
-ylabel('residue (Pa)')
+legend('observed', 'synthetic')
+ylabel('acoustic pressure (Pa)')
+title('pressure record')
 set(ax2, 'Box', 'on')
+% plot(dts_o', pres_o - circshift(s * pres_s, best_lags), 'k')
+% grid on
+% xlim(dt_ref_o + seconds(hdr_o.T0 + [-10 40]))
+% ylim([-1.1 1.1] * max(abs(pres_o - circshift(pres_s, best_lags))));
+% vline(ax2, dt_ref_o + seconds(hdr_o.T0), 'LineWidth', 2, ...
+%     'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
+% title('residue')
+% ylabel('residue (Pa)')
+% set(ax2, 'Box', 'on')
 
 % correlation coefficient
 ax3 = subplot('Position', [0.08 0.08 0.86 0.17]);
@@ -108,4 +137,12 @@ set(gcf, 'Renderer', 'painters')
 savename = sprintf('%s_%d_%s.eps', mfilename, hdr_o.USER7, ...
     replace(hdr_o.KSTNM, ' ', ''));
 figdisp(savename, [], [], 2, [], 'epstopdf');
+end
+
+function r = leq(a, b, ep)
+r = or(a < b, abs(a - b) < ep);
+end
+
+function r = geq(a, b, ep)
+r = or(a > b, abs(a - b) < ep);
 end
