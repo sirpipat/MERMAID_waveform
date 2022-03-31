c01a5053c6284f2bf1be37662b4ca1957e10f81e
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2022-03-31 11:19:02 -0400

    Slope in degree is atan not asin

--- bathymatter.m
+++ bathymatter.m
@@ -1,5 +1,8 @@
-function bathymatter(obsmasterdir, synmasterdir, flatmasterdir, bathmasterdir)
-% BATHYMATTER(obsmasterdir, synmasterdir, flatmasterdir, bathmasterdir)
+function [t_shifts, CCmaxs, depthstats, n, metadata] = ...
+    bathymatter(obsmasterdir, synmasterdir, flatmasterdir, ...
+    bathmasterdir, plt)
+% [t_shifts, CCmaxs, depthstats, n, metadata] = ...
+%     BATHYMATTER(obsmasterdir, synmasterdir, flatmasterdir, bathmasterdir)
 %
 % Compares 2 responses from flat ocean bottom and bathymetry from GEBCO in
 % order to determine whether bathymatry matters and when.
@@ -15,31 +18,101 @@ function bathymatter(obsmasterdir, synmasterdir, flatmasterdir, bathmasterdir)
 % bathmasterdir     the master directory to the output folders from
 %                   SPECFEM2D run for fluid-solid setting with GEBCO
 %                   bathymetry sorted into IRIS event ID folders
+% plt               whether to plot or not [Default: true]
+%
+% OUTPUT
+% t_shifts          Best time shift where CC is maximum     [flat, bath]
+% CCmaxs            Maximum correlation coefficient         [flat, bath]
+% depthstats        struct contatining the following fields
+%       - depth_mid         depth at the middle of the profile right below
+%                           the hydrophone
+%       - depth_avg         mean of the depth of the bathymetry profile
+%       - depth_std         standard deviation
+%       - depth_range       difference between the highest and the lowest
+%       - slope             slope in degree, positive when sloping upward
+% n                 the number of data points
+% metadata          SAC header variables sorted by variable names
 %
 % SEE ALSO
 % RUNFLATSIM, COMPARERESPONSEFUNCTIONS
 %
-% Last modified by sirawich-at-princeton.edu, 03/28/2022
+% Last modified by sirawich-at-princeton.edu, 03/31/2022
+
+defval('true', plt)
 
 [allflatdirs, fndex] = allfile(flatmasterdir);
 [allbathdirs, bndex] = allfile(bathmasterdir);
 
-% loop through allflatdirs
-for ii = 1:fndex
-    dir = removepath(allflatdirs{ii});
-    eventid = cindeks(split(dir, '_'), 2);
-    stationid = indeks(cindeks(split(dir, '_'), 3), 4:5);
-    obsfile = cindeks(ls2cell(sprintf('%s%s/*.%s_*.sac', ...
-        obsmasterdir, eventid, stationid), 1), 1);
-    synfile = cindeks(ls2cell(sprintf('%s%s/*_%s_0_*.sac', ...
-        synmasterdir, eventid, stationid), 1), 1);
-    try
-        compareresponsefunctions(obsfile, synfile, [allflatdirs{ii} '/'], ...
-            [allbathdirs{ii} '/']);
-    catch
-        continue
+defval('sname', sprintf('%s_%s.mat', mfilename, ...
+    hash([obsmasterdir synmasterdir flatmasterdir bathmasterdir ...
+    double(plt) fndex bndex], 'SHA-1')))
+
+pname = fullfile(getenv('IFILES'), 'HASHES', sname);
+
+if ~exist(pname, 'file')
+    CCmaxs_flat = [];
+    CCmaxs_bath = [];
+    t_shifts_flat = [];
+    t_shifts_bath = [];
+    depth_mid = [];
+    depth_avg = [];
+    depth_std = [];
+    depth_range = [];
+    slope = [];
+    fileused = {};
+    n = 1;
+
+    % loop through allflatdirs
+    for ii = 1:fndex
+        dir = removepath(allflatdirs{ii});
+        eventid = cindeks(split(dir, '_'), 2);
+        stationid = indeks(cindeks(split(dir, '_'), 3), 4:5);
+        obsfile = cindeks(ls2cell(sprintf('%s%s/*.%s_*.sac', ...
+            obsmasterdir, eventid, stationid), 1), 1);
+        synfile = cindeks(ls2cell(sprintf('%s%s/*_%s_0_*.sac', ...
+            synmasterdir, eventid, stationid), 1), 1);
+        try
+            [t_shift1, t_shift2, CCmax1, CCmax2, bath1, bath2] = ...
+                compareresponsefunctions(obsfile, synfile, ...
+                [allflatdirs{ii} '/'], [allbathdirs{ii} '/'], plt);
+            bath1(:,2) = bath1(:,2) - 9600;
+            bath2(:,2) = bath2(:,2) - 9600;
+            t_shifts_flat(n,1) = t_shift1;
+            t_shifts_bath(n,1) = t_shift2;
+            CCmaxs_flat(n,1) = CCmax1;
+            CCmaxs_bath(n,1) = CCmax2;
+            depth_mid(n,1) = bath2((size(bath2,1)+1)/2,2);
+            depth_avg(n,1) = mean(bath2(:,2));
+            depth_std(n,1) = std(bath2(:,2));
+            depth_range(n,1) = range(bath2(:,2));
+            slope(n,1) = atan(indeks(polyfit(bath2(:,1), ...
+                bath2(:,2), 1), 1)) * 180/pi;
+            fileused{n,1} = synfile;
+            n = n + 1;
+        catch ME
+            continue
+        end
     end
+
+    % fix the number of files
+    n = n - 1;
+
+    % gather stats
+    t_shifts = [t_shifts_flat, t_shifts_bath];
+    CCmaxs = [CCmaxs_flat, CCmaxs_bath];
+    depthstats = struct('depth_mid', depth_mid, 'depth_avg', depth_avg, ...
+        'depth_std', depth_std, 'depth_range', depth_range, 'slope', slope);
+
+    % gather metadata
+    metadata = getheaderarray(fileused);
+    
+    % save
+    fprintf('save the output to a file to %s ...\n', pname);
+    save(pname, 't_shifts', 'CCmaxs', 'depthstats', 'n', 'metadata');
+else
+    % load
+    fprintf('found the save in a file in %s\n', pname);
+    frpintf('load the variables ...\n');
+    load(pname, 't_shifts', 'CCmaxs', 'depthstats', 'n', 'metadata');
 end
-
-
 end
--- compareresponsefunctions.m
+++ compareresponsefunctions.m
@@ -1,5 +1,7 @@
-function compareresponsefunctions(obsfile, synfile, ddir1, ddir2)
-% compareresponsefunctions(obsfile, synfile, ddir1, ddir2)
+function [t_shift1, t_shift2, CCmax1, CCmax2, bath1, bath2] = ...
+    compareresponsefunctions(obsfile, synfile, ddir1, ddir2, plt)
+% [t_shift1, t_shift2, CCmax1, CCmax2, bath1, bath2] = ...
+%     compareresponsefunctions(obsfile, synfile, ddir1, ddir2, plt)
 %
 % Plot two synthetic pressure records obtained by convolving the synthetic
 % vertical displacement at the ocean bottom with the two response functions
@@ -13,10 +15,25 @@ function compareresponsefunctions(obsfile, synfile, ddir1, ddir2)
 %               from INSTASEIS
 % ddir1         directory to first  SPECFEM2D fluid-solid simulation
 % ddir2         directory to second SPECFEM2D fluid-solid simulation
+% plt           whether to plot or not [Defatult: true]
 %
 % OUTPUT
+% t_shift1      best time shifts where CCs are at maximum peak for first  
+%               SPECFEM2D fluid-solid simulation
+% t_shift2      best time shifts where CCs are at maximum peak for second 
+%               SPECFEM2D fluid-solid simulation
+% CCmax1        maximum correlation coefficients for first  SPECFEM2D 
+%               fluid-solid simulation
+% CCmax2        maximum correlation coefficients for second SPECFEM2D 
+%               fluid-solid simulation
+% bath1         bathymetry profile [x, z] for first  SPECFEM2D 
+%               fluid-solid simulation
+% bath2         bathymetry profile [x, z] for second SPECFEM2D 
+%               fluid-solid simulation
 %
-% Last modified by sirawich-at-princeton.edu, 03/28/2022
+% Last modified by sirawich-at-princeton.edu, 03/31/2022
+
+defval('plt', true)
 
 fcorners = [0.4 1];
 window_envelope = [-10 20];
@@ -59,9 +76,11 @@ seis_r2 = shannon(t_r2, seis_r2, t_r2_interp);
 % read bathymetry
 fname1 = cindeks(ls2cell(sprintf('%sDATA/interfaces*.dat', ddir1), 1), 1);
 [itfs1, ~] = loadinterfacefile(fname1);
+bath1 = itfs1{2}.pts;
 
 fname2 = cindeks(ls2cell(sprintf('%sDATA/interfaces*.dat', ddir2), 1), 1);
 [itfs2, ~] = loadinterfacefile(fname2);
+bath2 = itfs2{2}.pts;
 
 %% convolve
 pres_s1 = conv(seis_s_interp, seis_r1);
@@ -88,125 +107,127 @@ pres_s2 = bandpass(pres_s2, fs_o, fcorners(1), fcorners(2), 4, 2, ...
     dt_ref_o, fs_o, seconds(100), 'soft');
 
 %% plot
-% color
-red   = [0.9 0.0 0.0];
-blue  = [0.0 0.1 0.9];
-black = [0.4 0.4 0.4];
+if plt
+    % color
+    red   = [0.9 0.0 0.0];
+    blue  = [0.0 0.1 0.9];
+    black = [0.4 0.4 0.4];
 
-figure(3)
-set(gcf, 'Units', 'inches', 'Position', [9 8 6 8])
-clf
+    figure(3)
+    set(gcf, 'Units', 'inches', 'Position', [9 8 6 8])
+    clf
 
-% report
-ax0 = subplot(27,1,1);
-% criteria for CMT solution searching
-dt_origin = dt_ref_o + seconds(hdr_o.USER8);
-monthname = {'jan', 'feb', 'mar', 'apr', 'may', 'jun', ...
-    'jul', 'aug', 'sep', 'oct', 'nov', 'dec'};
-tbeg = datenum(dt_origin - minutes(1));
-tend = datenum(dt_origin + minutes(1));
-mblo = hdr_o.MAG - 0.5;
-mbhi = hdr_o.MAG + 0.5;
-depmin = hdr_o.EVDP - 50;
-depmax = hdr_o.EVDP + 50;
+    % report
+    ax0 = subplot(27,1,1);
+    % criteria for CMT solution searching
+    dt_origin = dt_ref_o + seconds(hdr_o.USER8);
+    monthname = {'jan', 'feb', 'mar', 'apr', 'may', 'jun', ...
+        'jul', 'aug', 'sep', 'oct', 'nov', 'dec'};
+    tbeg = datenum(dt_origin - minutes(1));
+    tend = datenum(dt_origin + minutes(1));
+    mblo = hdr_o.MAG - 0.5;
+    mbhi = hdr_o.MAG + 0.5;
+    depmin = hdr_o.EVDP - 50;
+    depmax = hdr_o.EVDP + 50;
 
-% get CMT solution to report
-fname = sprintf('%s%02d.ndk', monthname{dt_origin.Month}, ...
-    mod(dt_origin.Year, 100));
-[~,~,CMT] = readCMT(fname, strcat(getenv('IFILES'),'CMT'), tbeg, ...
-    tend, mblo, mbhi, depmin, depmax, 'hypocenter');
+    % get CMT solution to report
+    fname = sprintf('%s%02d.ndk', monthname{dt_origin.Month}, ...
+        mod(dt_origin.Year, 100));
+    [~,~,CMT] = readCMT(fname, strcat(getenv('IFILES'),'CMT'), tbeg, ...
+        tend, mblo, mbhi, depmin, depmax, 'hypocenter');
 
-titlestr = ['$$ \textnormal{CMT}-\textnormal{' ...
-            CMT.EventName ...
-            sprintf('}, M_w=%.2f', hdr_o.MAG) ...
-            ',~\Delta=' ...
-            sprintf('%.2f', hdr_o.GCARC) ...
-            '^{\circ},~\textnormal{depth} = ' ...
-            sprintf('%.2f', hdr_o.EVDP) ...
-            '~\textnormal{km} $$'];
-title(titlestr, 'Interpreter', 'latex')
+    titlestr = ['$$ \textnormal{CMT}-\textnormal{' ...
+                CMT.EventName ...
+                sprintf('}, M_w=%.2f', hdr_o.MAG) ...
+                ',~\Delta=' ...
+                sprintf('%.2f', hdr_o.GCARC) ...
+                '^{\circ},~\textnormal{depth} = ' ...
+                sprintf('%.2f', hdr_o.EVDP) ...
+                '~\textnormal{km} $$'];
+    title(titlestr, 'Interpreter', 'latex')
 
-% LaTeX string reporting the waveform timeshift finding
-txtstr = ['$$ \theta =' ...
-          sprintf('%.2f', asin(hdr_s.USER9 * 3400 / 6365000) * 180/pi) ...
-          '^{\circ}~\textnormal{, avg depth} = ' ...
-          sprintf('%.2f', 9600-mean(itfs2{2}.pts(:,2))) ...
-          '~\textnormal{m, std depth} = ' ...
-          sprintf('%.2f', std(itfs2{2}.pts(:,2))) ...
-          '~\textnormal{m, slope} = ' ...
-          sprintf('%.2f^{\\circ} $$', ...
-          asin(indeks(polyfit(itfs2{2}.pts(:,1), ...
-          itfs2{2}.pts(:,2), 1), 1)) * 180/pi)];
-[x_pos, y_pos] = norm2trueposition(ax0, -3/40, 3/4);
-text(x_pos, y_pos, txtstr, 'FontSize', 12, 'Interpreter', 'latex');
+    % LaTeX string reporting the waveform timeshift finding
+    txtstr = ['$$ \theta =' ...
+              sprintf('%.2f', asin(hdr_s.USER9 * 3400 / 6365000) * 180/pi) ...
+              '^{\circ}~\textnormal{, avg depth} = ' ...
+              sprintf('%.2f', 9600-mean(itfs2{2}.pts(:,2))) ...
+              '~\textnormal{m, std depth} = ' ...
+              sprintf('%.2f', std(itfs2{2}.pts(:,2))) ...
+              '~\textnormal{m, slope} = ' ...
+              sprintf('%.2f^{\\circ} $$', ...
+              atan(indeks(polyfit(itfs2{2}.pts(:,1), ...
+              itfs2{2}.pts(:,2), 1), 1)) * 180/pi)];
+    [x_pos, y_pos] = norm2trueposition(ax0, -3/40, 3/4);
+    text(x_pos, y_pos, txtstr, 'FontSize', 12, 'Interpreter', 'latex');
 
-set(ax0, 'FontSize', 12, 'Color', 'none');
-ax0.XAxis.Visible = 'off';
-        ax0.YAxis.Visible = 'off';
+    set(ax0, 'FontSize', 12, 'Color', 'none');
+    ax0.XAxis.Visible = 'off';
+            ax0.YAxis.Visible = 'off';
 
-% bathymetry
-ax1 = subplot(27,1,[2,8]);
-[ax1, hs] = drawbackground(fname2, ax1);
-ax1.YTick = 9600 - (8000:-2000:0);
-ax1.YTickLabel = string(ax1.YTick-9600);
-hold on
-vline(ax1, 5000*(1:3), 'LineWidth', 0.5, 'LineStyle', ':', 'Color', [1 1 0.5]);
-hline(ax1, ax1.YTick, 'LineWidth', 0.5, 'LineStyle', ':', 'Color', [1 1 0.5]);
-plot(itfs2{2}.pts(:,1), itfs2{2}.pts(:,2), 'LineStyle', '-', 'LineWidth', 2, 'Color', [0.6 1 1])
-plot(itfs1{2}.pts(:,1), itfs1{2}.pts(:,2), 'LineStyle', '--', 'LineWidth', 2, 'Color', 'r')
-xlabel('x (m)')
-ylabel('elevation (m)')
-%title('bathymetry')
+    % bathymetry
+    ax1 = subplot(27,1,[2,8]);
+    [ax1, hs] = drawbackground(fname2, ax1);
+    ax1.YTick = 9600 - (8000:-2000:0);
+    ax1.YTickLabel = string(ax1.YTick-9600);
+    hold on
+    vline(ax1, 5000*(1:3), 'LineWidth', 0.5, 'LineStyle', ':', 'Color', [1 1 0.5]);
+    hline(ax1, ax1.YTick, 'LineWidth', 0.5, 'LineStyle', ':', 'Color', [1 1 0.5]);
+    plot(itfs2{2}.pts(:,1), itfs2{2}.pts(:,2), 'LineStyle', '-', 'LineWidth', 2, 'Color', [0.6 1 1])
+    plot(itfs1{2}.pts(:,1), itfs1{2}.pts(:,2), 'LineStyle', '--', 'LineWidth', 2, 'Color', 'r')
+    xlabel('x (m)')
+    ylabel('elevation (m)')
+    %title('bathymetry')
 
-% response function
-ax2 = subplot(27,1,[11,14]);
-plot(t_r1, seis_r1 / max(abs(seis_r1)) + 1, 'LineWidth', 1, 'Color', red)
-hold on
-grid on
-plot(t_r2, seis_r2 / max(abs(seis_r1)) - 1, 'LineWidth', 1, 'Color', blue)
-ylim([-2 2])
-legend('flat', 'bathymetry', 'location', 'east')
-xlabel('time (s)')
-ylabel('response')
-title('response function', 'Interpreter', 'latex', 'FontSize', 11)
-set(ax2, 'Box', 'on')
+    % response function
+    ax2 = subplot(27,1,[11,14]);
+    plot(t_r1, seis_r1 / max(abs(seis_r1)) + 1, 'LineWidth', 1, 'Color', red)
+    hold on
+    grid on
+    plot(t_r2, seis_r2 / max(abs(seis_r1)) - 1, 'LineWidth', 1, 'Color', blue)
+    ylim([-2 2])
+    legend('flat', 'bathymetry', 'location', 'east')
+    xlabel('time (s)')
+    ylabel('response')
+    title('response function', 'Interpreter', 'latex', 'FontSize', 11)
+    set(ax2, 'Box', 'on')
 
-ax3 = subplot(27,1,[17,18]);
-plot(t_relative, bandpass(seis_s_interp, fs_o, fcorners(1), ...
-    fcorners(2), 4, 2, 'butter', 'linear'), 'LineWidth', 1, 'Color', black)
-hold on
-grid on
-xlim([-10 25])
-vline(ax3, 0, 'LineWidth', 2, 'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
-xlabel('time since first picked arrival (s)')
-ylabel('u_z (m)')
-title(sprintf('synthetic vertical displacement: bp%.1f-%.1f', fcorners(1), ...
-    fcorners(2)), 'Interpreter', 'latex', 'FontSize', 11)
-set(ax3, 'Box', 'on')
+    ax3 = subplot(27,1,[17,18]);
+    plot(t_relative, bandpass(seis_s_interp, fs_o, fcorners(1), ...
+        fcorners(2), 4, 2, 'butter', 'linear'), 'LineWidth', 1, 'Color', black)
+    hold on
+    grid on
+    xlim([-10 25])
+    vline(ax3, 0, 'LineWidth', 2, 'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
+    xlabel('time since first picked arrival (s)')
+    ylabel('u_z (m)')
+    title(sprintf('synthetic vertical displacement: bp%.1f-%.1f', fcorners(1), ...
+        fcorners(2)), 'Interpreter', 'latex', 'FontSize', 11)
+    set(ax3, 'Box', 'on')
 
-ax4 = subplot(27,1,[22,27]);
-plot(t_relative, pres_o, 'LineWidth', 1, 'Color', black)
-hold on
-grid on
-plot(t_relative + t_shift1, pres_s1 * s1, 'LineWidth', 1, 'Color', red)
-plot(t_relative + t_shift2, pres_s2 * s2, 'LineWidth', 1, 'Color', blue)
-xlim([-10 25])
-%ylim([-1.1 1.1] * max(max(abs(pres_o)), max(abs(s_e * pres_s1))))
-vline(ax4, 0, 'LineWidth', 2, 'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
-legend('observed', ...
-    sprintf('flat : \\tau^W = %.2f s, X^W = %.2f', t_shift1, CCmax1), ...
-    sprintf('bathymetry : \\tau^W = %.2f s, X^W = %.2f', t_shift2, CCmax2), ...
-    'Location', 'southoutside', 'Interpreter', 'latex')
-xlabel('time since first picked arrival (s)')
-ylabel('P (Pa)')
-title(sprintf(['acoustic pressure record: bp%.1f-%.1f W$^E$[%d %d]' ...
-    ' W$^W$[%d %d]'], fcorners(1), fcorners(2), window_envelope(1), ...
-    window_envelope(2), window_waveform(1), window_waveform(2)), ...
-    'Interpreter', 'latex', 'FontSize', 11)
-set(ax4, 'Box', 'on')
+    ax4 = subplot(27,1,[22,27]);
+    plot(t_relative, pres_o, 'LineWidth', 1, 'Color', black)
+    hold on
+    grid on
+    plot(t_relative + t_shift1, pres_s1 * s1, 'LineWidth', 1, 'Color', red)
+    plot(t_relative + t_shift2, pres_s2 * s2, 'LineWidth', 1, 'Color', blue)
+    xlim([-10 25])
+    %ylim([-1.1 1.1] * max(max(abs(pres_o)), max(abs(s_e * pres_s1))))
+    vline(ax4, 0, 'LineWidth', 2, 'LineStyle', '--', 'Color', [0.1 0.8 0.1]);
+    legend('observed', ...
+        sprintf('flat : \\tau^W = %.2f s, X^W = %.2f', t_shift1, CCmax1), ...
+        sprintf('bathymetry : \\tau^W = %.2f s, X^W = %.2f', t_shift2, CCmax2), ...
+        'Location', 'southoutside', 'Interpreter', 'latex')
+    xlabel('time since first picked arrival (s)')
+    ylabel('P (Pa)')
+    title(sprintf(['acoustic pressure record: bp%.1f-%.1f W$^E$[%d %d]' ...
+        ' W$^W$[%d %d]'], fcorners(1), fcorners(2), window_envelope(1), ...
+        window_envelope(2), window_waveform(1), window_waveform(2)), ...
+        'Interpreter', 'latex', 'FontSize', 11)
+    set(ax4, 'Box', 'on')
 
-set(gcf, 'Renderer', 'painters')
-savename = sprintf('%s_%d_%s.eps', mfilename, hdr_o.USER7, ...
-    replace(hdr_o.KSTNM, ' ', ''));
-figdisp(savename, [], [], 2, [], 'epstopdf');
+    set(gcf, 'Renderer', 'painters')
+    savename = sprintf('%s_%d_%s.eps', mfilename, hdr_o.USER7, ...
+        replace(hdr_o.KSTNM, ' ', ''));
+    figdisp(savename, [], [], 2, [], 'epstopdf');
+end
 end
