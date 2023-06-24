ceec0f92eb6e36ddc61d1c4eddefe9819b7b5cef
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2023-06-24 18:00:16 -0400

    Renamed figure number + reordered and recolored plots

--- pp2023figure8.m
+++ pp2023figure8.m
@@ -6,104 +6,100 @@ function varargout = pp2023figure8
 % OUTPUT:
 % fig       figure handle to the plots
 %
-% Last modified by sirawich-at-princeton.edu: 04/05/2023
-
+% Last modified by sirawich-at-princeton.edu: 06/25/2023
 %% load data
 
-% load INSTASEIS displacement seismogram at the ocean bottom
-[seis_s, hdr_s] = readsac(sprintf(...
-    '%sDATA/Figure8/20190115T180634_09_0_SYNTHETIC.sac', ...
-    getenv('MERMAID2')));
-[dt_ref_s, dt_begin_s, ~, fs_s, ~, dts_s] = gethdrinfo(hdr_s);
+% SPECFEM-vertical displacement (template)
+ddir = sprintf('%sDATA/Figure8/bathymetry/', getenv('MERMAID2'));
+[t_s, x_s] = getarrivaltemplate(ddir, 'bottom');
 
-% read seismograms from SPECFEM2D simulation
-ddir = sprintf('%sDATA/Figure7/bathymetry/', getenv('MERMAID2'));
-file_s = [ddir 'OUTPUT_FILES/AB.S0001.BXZ.semd'];
-[t_s, x_s] = read_seismogram(file_s);
-
-% read the observed pressure record
-[seis_o, hdr_o] = readsac(sprintf(...
-    '%sDATA/Figure8/20190115T181021.09_5C3E73D6.MER.DET.WLT5.sac', ...
-    getenv('MERMAID2')));
-[dt_ref_o, dt_begin_o, ~, fs_o, ~, dts_o] = gethdrinfo(hdr_o);
+% SPECFEM-pressure
+file_p = [ddir 'OUTPUT_FILES/AA.S0001.PRE.semp'];
+[t_p, x_p] = read_seismogram(file_p);
 
 %% calculations
-
-% sampling rate of the seiemograms from SPECFEM2D simulation
 fs = 1 / (t_s(2) - t_s(1));
 
-% deconvolve for the response function due to the ocean layer
-[~, ~, t_r, seis_r] = cctransplot(ddir, ddir, 'flat_10996154_P0009', ...
+% response function
+[t_cc, cc, t_rf, rf, d] = cctransplot(ddir, ddir, 'flat_10996154_P0009', ...
     {'bottom', 'displacement'}, {'hydrophone', 'pressure'}, [], fs, false);
 
-% time relative to the first P-wave arrival
-t_relative = seconds(dts_o - dt_ref_o) - hdr_o.T0;
-
-
-% remove instrument response and filter
-pres_o = counts2pa(seis_o, fs_o, [0.05 0.1 10 20], [], 'sacpz', false);
-pres_o = real(pres_o);
-
-% determine the corner frequency
-%[fcorners, ~] = freqselect(t_relative, pres_o, fs_o, false);
-
-% filter
-fcorners = [0.85 1.45];
-pres_o = bandpass(pres_o, fs_o, fcorners(1), fcorners(2), 4, 2, 'butter', 'linear');
-
-% resample to MERMAID datetimes
-seis_s_interp = shannon(dts_s, seis_s, dts_o);
-t_r_interp = 0:(1/fs_o):t_r(end);
-seis_r = shannon(t_r, seis_r, t_r_interp);
-
-% convolve for the synthetic pressure
-pres_s = conv(seis_s_interp, seis_r);
-pres_s = pres_s(1:length(seis_o), 1);
-pres_s = bandpass(pres_s, fs_o, fcorners(1), fcorners(2), 4, 2, ...
-    'butter', 'linear');
-
-% filter
-seis_s_interp = bandpass(seis_s_interp, fs_o, fcorners(1), fcorners(2), ...
-    4, 2, 'butter', 'linear');
+% convolve
+x_p_conv = conv(x_s, rf);
+x_p_conv = x_p_conv(1:length(x_p));
 
 %% plot
 figure(8)
-set(gcf, 'Units', 'inches', 'Position', [0 1 8 3.5])
+set(gcf, 'Units', 'inches', 'Position', [0 1 8 4])
 clf
 
-ax1 = subplot('Position', [0.04 0.20 0.92 0.76]);
-plot(ax1, t_relative, seis_s_interp / max(abs(seis_s_interp)) + 4, 'LineWidth', 1, 'Color', 'k')
+ax1 = subplot('Position', [0.09 0.74 0.88 0.24]);
+plot(ax1, t_p, x_p_conv / max(abs(x_p)), 'r', 'LineWidth', 1.4)
 hold on
-plot(ax1, t_relative, pres_s / max(abs(pres_s)) + 2, 'LineWidth', 1, 'Color', 'k')
-plot(ax1, t_relative, pres_o / max(abs(pres_o)), 'LineWidth', 1, 'Color', 'k')
+plot(ax1, t_p, x_p / max(abs(x_p)), 'b', 'LineWidth', 1)
 grid on
+xlim([0, 30])
+ylim([-1.5, 1.5])
+nolabels(ax1, 1)
 
-% label traces
-text(ax1, -9, 4.6, 'synthetic displacement ($$s$$)', ...
-    'FontSize', 16, 'Interpreter', 'latex')
-text(ax1, -9, 2.6, 'synthetic pressure ($$\hat{p} = s*r$$)', ...
-    'FontSize', 16, 'Interpreter', 'latex')
-text(ax1, -9, 0.8, 'observed pressure', ...
-    'FontSize', 16, 'Interpreter', 'latex')
+% label graph
+text(ax1, 19.5, 0.9, 'pressure at hydrophone ($$p$$)', ...
+    'FontSize', 14, 'Color', 'b', 'Interpreter', 'latex')
+text(ax1, 13, -1.0, 'estimated pressure at hydrophone ($$\hat{p} = s*r$$)', ...
+    'FontSize', 14, 'Color', 'r', 'Interpreter', 'latex')
 
 % label scale
-scalelabel = sprintf('( $$\\tims %.2g}$$ m )', max(abs(seis_s_interp)));
-scalelabel = replace(scalelabel, 'e', '\times 10^{');
-scalelabel = replace(scalelabel, 'tims', 'times');
-scalelabel = replace(scalelabel, '-0', '-');
-text(ax1, 5, 4.6, scalelabel, 'FontSize', 14, 'Interpreter', 'latex')
-scalelabel = sprintf('( $$\\times %.2g$$ Pa )', max(abs(pres_s)));
-text(ax1, 5, 2.6, scalelabel, 'FontSize', 14, 'Interpreter', 'latex')
-scalelabel = sprintf('( $$\\times %.2g$$ Pa )', max(abs(pres_o)));
-text(ax1, 5, 0.8, scalelabel, 'FontSize', 14, 'Interpreter', 'latex')
+scalelabel = sprintf('%.2g}', max(abs(x_p)));
+scalelabel = replace(scalelabel, 'e', '\times10^{');
+scalelabel = replace(scalelabel, 'x', '\times');
+yticklabels({['-' scalelabel], 0 ,scalelabel})
 
-xlim([-10 25])
-ylim([-1.5 5.5])
-yticks([0 2 4])
-yticklabels({'', '', ''})
+set(ax1, 'Box', 'on', 'FontSize', 12, 'TickDir', 'out')
+
+boxedlabel(ax1, 'northwest', 0.28, [], 'a', 'FontSize', 14);
+
+ax2 = subplot('Position', [0.09 0.44 0.88 0.24]);
+plot(ax2, t_s, x_s / max(abs(x_s)), 'Color', [0 0.4 0], 'LineWidth', 1)
+grid on
+xlim([0, 30])
+ylim([-1.2, 1.2])
+nolabels(ax2, 1)
+
+% label graph
+text(ax2, 17, 0.4, 'displacement at ocean bottom ($$s$$)', ...
+    'FontSize', 14, 'Color', [0 0.4 0], 'Interpreter', 'latex')
+
+% label scale
+scalelabel = sprintf('%.2g}', max(abs(x_s)));
+scalelabel = replace(scalelabel, 'e', '\times10^{');
+scalelabel = replace(scalelabel, 'x', '\times');
+yticklabels({['-' scalelabel], 0 ,scalelabel})
+
+set(ax2, 'Box', 'on', 'FontSize', 12, 'TickDir', 'out')
+
+boxedlabel(ax2, 'northwest', 0.28, [], 'b', 'FontSize', 14);
+
+ax3 = subplot('Position', [0.09 0.14 0.88 0.24]);
+plot(ax3, t_rf, rf / max(abs(rf)), 'k', 'LineWidth', 1)
+grid on
+xlim([0, 30])
+ylim([-1.5, 1.5])
 xlabel('time (s)')
 
-set(ax1, 'Box', 'on', 'TickDir', 'out', 'FontSize', 12)
+% label graph
+text(ax3, 19.3, 1.05, 'response function ($$r = s/p$$)', ...
+    'FontSize', 14, 'Interpreter', 'latex')
+
+% label scale
+scalelabel = sprintf('%.2g}', max(abs(rf)));
+scalelabel = replace(scalelabel, 'e', '\times10^{');
+scalelabel = replace(scalelabel, '+0', '');
+scalelabel = replace(scalelabel, 'x', '\times');
+yticklabels({['-' scalelabel], 0 ,scalelabel})
+
+set(ax3, 'Box', 'on', 'FontSize', 12, 'TickDir', 'out')
+
+boxedlabel(ax3, 'northwest', 0.28, [], 'c', 'FontSize', 14);
 
 set(gcf, 'Renderer', 'painters')
 
---
+++ pp2023figure9.m
@@ -0,0 +1,161 @@
+function varargout = pp2023figure9
+% fig = PP2023FIGURE9
+%
+% Makes figure 9 of Pipatprathanporn+2023
+%
+% OUTPUT:
+% fig       figure handle to the plots
+%
+% Last modified by sirawich-at-princeton.edu: 06/25/2023
+
+%% load data
+
+% load INSTASEIS displacement seismogram at the ocean bottom
+[seis_s, hdr_s] = readsac(sprintf(...
+    '%sDATA/Figure9/20190115T180634_09_0_SYNTHETIC.sac', ...
+    getenv('MERMAID2')));
+[dt_ref_s, dt_begin_s, ~, fs_s, ~, dts_s] = gethdrinfo(hdr_s);
+
+% read seismograms from SPECFEM2D simulation
+ddir = sprintf('%sDATA/Figure8/bathymetry/', getenv('MERMAID2'));
+file_s = [ddir 'OUTPUT_FILES/AB.S0001.BXZ.semd'];
+[t_s, x_s] = read_seismogram(file_s);
+
+% read the observed pressure record
+[seis_o, hdr_o] = readsac(sprintf(...
+    '%sDATA/Figure9/20190115T181021.09_5C3E73D6.MER.DET.WLT5.sac', ...
+    getenv('MERMAID2')));
+[dt_ref_o, dt_begin_o, ~, fs_o, ~, dts_o] = gethdrinfo(hdr_o);
+
+%% calculations
+
+% sampling rate of the seiemograms from SPECFEM2D simulation
+fs = 1 / (t_s(2) - t_s(1));
+
+% deconvolve for the response function due to the ocean layer
+[~, ~, t_r, seis_r] = cctransplot(ddir, ddir, 'flat_10996154_P0009', ...
+    {'bottom', 'displacement'}, {'hydrophone', 'pressure'}, [], fs, false);
+
+% time relative to the first P-wave arrival
+t_relative = seconds(dts_o - dt_ref_o) - hdr_o.T0;
+
+
+% remove instrument response and filter
+pres_o = counts2pa(seis_o, fs_o, [0.05 0.1 10 20], [], 'sacpz', false);
+pres_o = real(pres_o);
+
+% determine the corner frequency
+%[fcorners, ~] = freqselect(t_relative, pres_o, fs_o, false);
+
+% filter
+fcorners = [0.50 1.30];
+pres_o = bandpass(pres_o, fs_o, fcorners(1), fcorners(2), 4, 2, 'butter', 'linear');
+
+% timeshift
+timeshift = -0.6998;
+
+% resample to MERMAID datetimes
+seis_s_interp = shannon(dts_s, seis_s, dts_o);
+t_r_interp = 0:(1/fs_o):t_r(end);
+seis_r = shannon(t_r, seis_r, t_r_interp);
+
+% convolve for the synthetic pressure
+pres_s = conv(seis_s_interp, seis_r);
+pres_s = pres_s(1:length(seis_o), 1);
+pres_s = bandpass(pres_s, fs_o, fcorners(1), fcorners(2), 4, 2, ...
+    'butter', 'linear');
+
+% filter
+seis_s_interp = bandpass(seis_s_interp, fs_o, fcorners(1), fcorners(2), ...
+    4, 2, 'butter', 'linear');
+
+%% plot
+figure(9)
+set(gcf, 'Units', 'inches', 'Position', [0 1 8 4])
+clf
+
+ax1 = subplot('Position', [0.09 0.74 0.88 0.24]);
+plot(ax1, t_relative, pres_o / max(abs(pres_s)), 'LineWidth', 1.4, 'Color', 'b')
+hold on
+plot(ax1, t_relative + timeshift, pres_s / max(abs(pres_s)), 'LineWidth', 1, 'Color', 'r')
+grid on
+xlim([-10 35])
+ylim([-1.5, 1.5])
+nolabels(ax1, 1)
+
+% label graph
+text(ax1, -7.5, -0.9, 'synthetic pressure ($$\hat{p} = s*r$$)', ...
+    'FontSize', 14, 'Color', 'r', 'Interpreter', 'latex')
+text(ax1, -7.5, 1.0, 'observed pressure ($$p$$)', ...
+    'FontSize', 14, 'Color', 'b', 'Interpreter', 'latex')
+
+% label scale
+scalelabel = sprintf('%.2g', max(abs(pres_s)));
+if contains(scalelabel, 'e')
+    scalelabel = [scalelabel '}'];
+    scalelabel = replace(scalelabel, 'e', '\times10^{');
+    scalelabel = replace(scalelabel, 'x', '\times');
+end
+yticklabels({['-' scalelabel], 0 ,scalelabel})
+
+set(ax1, 'Box', 'on', 'FontSize', 12, 'TickDir', 'out')
+boxedlabel(ax1, 'northwest', 0.28, [], 'a', 'FontSize', 14);
+
+ax2 = subplot('Position', [0.09 0.44 0.88 0.24]);
+plot(ax2, t_relative, seis_s_interp / max(abs(seis_s_interp)), 'LineWidth', 1, 'Color', [0 0.4 0])
+grid on
+xlim([-10 35])
+ylim([-1.5, 1.5])
+nolabels(ax2, 1)
+
+% label graph
+text(ax2, -7.5, 1.0, 'synthetic displacement ($$s$$)', ...
+    'FontSize', 14, 'Color', [0 0.4 0], 'Interpreter', 'latex')
+
+% label scale
+scalelabel = sprintf('%.2g', max(abs(seis_s_interp)));
+if contains(scalelabel, 'e')
+    scalelabel = [scalelabel '}'];
+    scalelabel = replace(scalelabel, 'e', '\times10^{');
+    scalelabel = replace(scalelabel, 'x', '\times');
+end
+yticklabels({['-' scalelabel], 0 ,scalelabel})
+
+set(ax2, 'Box', 'on', 'FontSize', 12, 'TickDir', 'out')
+boxedlabel(ax2, 'northwest', 0.28, [], 'b', 'FontSize', 14);
+
+ax3 = subplot('Position', [0.09 0.14 0.88 0.24]);
+plot(ax3, [-10 -5 -1 t_r_interp 33 35], ...
+    [0; 0; 0; seis_r / max(abs(seis_r)); 0; 0], 'k', 'LineWidth', 1)
+grid on
+xlim([-10 35])
+ylim([-1.5, 1.5])
+xlabel('time (s)')
+
+% label graph
+text(ax3, -7.5, 1.05, 'response function ($$r$$)', ...
+    'FontSize', 14, 'Interpreter', 'latex')
+
+% label scale
+scalelabel = sprintf('%.2g', max(abs(seis_r)));
+if contains(scalelabel, 'e')
+    scalelabel = [scalelabel '}'];
+    scalelabel = replace(scalelabel, 'e', '\times10^{');
+    scalelabel = replace(scalelabel, 'x', '\times');
+end
+yticklabels({['-' scalelabel], 0 ,scalelabel})
+
+set(ax3, 'Box', 'on', 'FontSize', 12, 'TickDir', 'out')
+
+boxedlabel(ax3, 'northwest', 0.28, [], 'c', 'FontSize', 14);
+
+set(gcf, 'Renderer', 'painters')
+
+%% save figure
+figname = sprintf('%s.eps', mfilename);
+figdisp(figname, [], [], 2, [], 'epstopdf');
+
+if nargout > 0
+    varargout{1} = fig;
+end
+end
