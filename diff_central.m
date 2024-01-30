51c6da9c7cdd6b4631c4e08448f1ab5eb0f63db1
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2024-01-30 17:14:11 -0500

    Update documentation

--- diff_central.m
+++ diff_central.m
@@ -1,4 +1,6 @@
 function g = diff_central(f, h)
+% g = DIFF_CENTRAL(f, h)
+%
 % Compute the derivative of f(x) using first-order central finite 
 % difference method on a uniform mesh.
 %
@@ -8,7 +10,7 @@ function g = diff_central(f, h)
 % OUTPUT
 % g         f'(x)
 %
-% Last modified by Sirawich Pipatprathanporn, 02/17/2021
+% Last modified by Sirawich Pipatprathanporn, 01/30/2024
 
 g = (circshift(f,-1) - circshift(f,1)) / 2 / h;
 g(1) = (f(2) - f(1)) / h;
